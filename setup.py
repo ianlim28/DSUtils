c74e88a8e5848ce28759834e95f3d9f2e7a958cb
Ian Lim <ianlim28@hotmail.com>
2020-05-13 16:56:27 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(name='dsutils',
       version='0.1',
       description='Basic utilities for ian data science',
-      url='https://github.com/brendanhasz/DSUtils',
+      url='https://github.com/ianlim28/DSUtils',
       author='ianlim',
       author_email='ianlim28@hotmail.com',
       license='MIT',
